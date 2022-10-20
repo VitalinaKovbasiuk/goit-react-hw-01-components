ffd02eafc8b2a6ecdfd7d639697a8e0470298aae
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-10-20 23:10:17 +0300

    task 2

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import user from "../data/user.json";
+import Profile from "./Profile/Profile";
+
+import data from "../data/data.json";
+import Statistics from "./Statistics/Statistics";
+
+function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
+  );
+}
+
+export default App;
